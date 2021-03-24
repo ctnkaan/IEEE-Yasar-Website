fad08d9d49de18ce5a35d7f9f3cd8fef978981c7
mcig <cig-mustafa.2001@hotmail.com>
2021-03-24 12:17:11 +0300

    added a bit glitchy coinflip anim

--- src/components/Home/LogoBall.js
+++ src/components/Home/LogoBall.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "../../styles/CommitteesSection.module.css";
 import { Link } from "react-router-dom";
 function LogoBall({ logo, alt, toSlug, animationType }) {
+  const [isHovering, setIsHovering] = useState(false);
+  const [oneTimeOnlyFlag, setOneTimeOnlyFlag] = useState(true);
   return (
     <Link
-      className={`wow animate__animated animate__${animationType}`}
-      data-wow-delay="0.2s"
+      className={`wow animate__animated animate__${
+        isHovering ? "flip" : oneTimeOnlyFlag && animationType
+      }`}
       to={toSlug && `/society/${toSlug}`}
       style={{ color: "inherit" }}
+      onMouseEnter={() => {
+        if (!isHovering) {
+          setIsHovering(true);
+          setOneTimeOnlyFlag(false);
+        }
+      }}
+      onMouseLeave={() => {
+        if (isHovering) {
+          setIsHovering(false);
+          setOneTimeOnlyFlag(false);
+        }
+      }}
     >
       <div className={classes.logoBorderAnim}>
         <img
