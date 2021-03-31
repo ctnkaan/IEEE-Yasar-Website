a3484a8ee6f040804e1ffccc0eb37bbae4eb01f4
mcig <cig-mustafa.2001@hotmail.com>
2021-03-31 10:27:47 +0300

    added event cards

---
+++ src/resources/getEventDatas.js
@@ -0,0 +1,65 @@
+import git from "../resources/eventImages/eventcs.jpg";
+
+export default function getEventDatas() {
+  //2d array with at most 4 jsons in each array
+  return [
+    //first row
+    [
+      {
+        title: "Git ve Github 101",
+        description: `Çetin Kaan Taşkıngenç tarafından verilen eğitici ve bir
+         o kadar önemli olan bu etkinliğimizde geforce now türkiyeye geldi kekw`,
+        image: git, //must be imported above
+        date: "07/03/2021",
+      },
+      {
+        title: "title2",
+        description: "",
+        image: "",
+        date: "",
+      },
+      {
+        title: "title3",
+        description: "",
+        image: "",
+        date: "",
+      },
+      {
+        title: "title4",
+        description: "",
+        image: "",
+        date: "",
+      },
+    ],
+
+    //second row
+    [
+      {
+        title: "title",
+        description: "",
+        image: "",
+        date: "",
+      },
+      {
+        title: "title2",
+        description: "",
+        image: "",
+        date: "",
+      },
+      {
+        title: "title2",
+        description: "",
+        image: "",
+        date: "",
+      },
+      {
+        title: "title2",
+        description: "",
+        image: "",
+        date: "",
+      },
+    ],
+
+    //add new rows correspondingly...
+  ];
+}
--- src/routes/Events.js
+++ src/routes/Events.js
@@ -1,7 +1,28 @@
 import React from "react";
-
+import { Card, CardDeck, Container } from "react-bootstrap";
+import getEventDatas from "../resources/getEventDatas";
 function Events() {
-  return <div style={{ marginTop: "90px" }}>Etkinliklerimiz</div>;
+  const events2d = getEventDatas(); //returns 2d array
+  return (
+    <Container style={{ marginTop: "90px" }}>
+      {events2d.map((events) => (
+        <CardDeck className="mt-5">
+          {events.map((event) => (
+            <Card>
+              <Card.Img variant="top" src={event.image} />
+              <Card.Body>
+                <Card.Title>{event.title}</Card.Title>
+                <Card.Text>{event.description}</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">{event.date}</small>
+              </Card.Footer>
+            </Card>
+          ))}
+        </CardDeck>
+      ))}
+    </Container>
+  );
 }
 
 export default Events;
