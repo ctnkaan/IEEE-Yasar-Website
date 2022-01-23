19304d7a463296d8192962dfa682217397f81435
ibrahim halil sakli <63147096+softwareVirus@users.noreply.github.com>
2022-01-23 18:17:43 +0300

    Update Header.js

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import HeaderButton from './HeaderButton'
 import { header } from './Profile'
-import '../styles/Header.css'
+import '../../styles/Header.css'
 const Header = () => {
 
     return (
